143fc1a810905b1b8b1d87a332936e00b0d53434
Nicole Gunn <nicole@clockworkproperty.co.nz>
2024-05-27 09:05:19 +1200

    update package.json

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express();
 const cors = require("cors");
 
 const allowedOrigins = [
+  // Add any frontend urls that will need to access the apis
   "https://red-mud-0cb8e9600.5.azurestaticapps.net",
-  `http://localhost:${process.env.PORT}`,
+  "http://localhost:5173", //vite on local
 ];
 
 const corsOptions = {
