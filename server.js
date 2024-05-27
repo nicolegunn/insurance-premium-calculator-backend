ddef5e72fe13763e8cd3cdcbd3d3a0088eadf6b5
Nicole Gunn <nicole@clockworkproperty.co.nz>
2024-05-27 12:52:16 +1200

    add two tests and code to pass for risk_rating api

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ require("dotenv").config();
 const app = require("./index.js");
 
 const PORT = process.env.PORT || 4000;
-app
-  .listen(PORT, () => {
-    console.log(`Server is alive on http://localhost:${PORT}`);
-  })
-  .on("error", (error) => {
-    if (error.code === "EADDRINUSE") {
-      console.log(`PORT ${PORT} is already in use`);
-    } else {
-      console.log(`Server Error`, error);
-    }
-  });
+const server = app.listen(PORT, () => {
+  console.log(`Server is alive on http://localhost:${PORT}`);
+});
+
+server.on("error", (error) => {
+  if (error.code === "EADDRINUSE") {
+    console.log(`PORT ${PORT} is already in use`);
+  } else {
+    console.log(`Server Error`, error);
+  }
+});
+
+module.exports = server;
