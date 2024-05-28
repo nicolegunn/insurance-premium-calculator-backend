12048a019ec3896ec4a5bc7fe7e5d41f8b60f267
Pete Mackay <greenworxnz@gmail.com>
2024-05-28 17:38:01 +1200

    working api on the /value endpoint

--- __tests__/value.test.js
+++ __tests__/value.test.js
@@ -1,5 +1,5 @@
-// const request = require("supertest");
-// const app = require("../server.js");
+const request = require("supertest");
+//const app = require("../server.js");
 
 const validateVehicle = require("../controllers/valueController");
 
